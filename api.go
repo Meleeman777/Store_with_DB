2d515bf848b93fe536e2a8d24b0c0a19beee67a4
Ivan <abramenkoiv@nspk.ru>
2023-09-06 23:22:52 +0300

    init commit

---
+++ api.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+    "encoding/json"
+    "database/sql"
+    "net/http"
+    "fmt"
+    "log"
+    _ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+type Product struct {
+	Id int
+	Name string
+	Price int
+	Description string
+	Category string
+}
+type Products struct {
+	Products []Product
+}
+func main() {
+	var err error
+
+	db, err = sql.Open("postgres", "host=127.0.0.1 user=api password=123456 dbname=api sslmode=disable")
+        if err != nil {
+            panic(err)
+        }
+
+	defer db.Close()
+
+	fmt.Println("Starting...")
+	http.HandleFunc("/v2/products/:id", changeProduct)
+	http.HandleFunc("/v2/products/", getProducts)
+	log.Fatal(http.ListenAndServe(":7070", nil))
+
+}
+func changeProduct(w http.ResponseWriter, r *http.Request) {
+        if r.Method != "PUT" {
+                http.Error(w, "Method Not Allowed", 405)
+        } else {
+		decoder := json.NewDecoder(r.Body)
+		var g_product Product
+
+		err := decoder.Decode(&g_product)
+		if err != nil {
+			panic(err)
+		}
+
+		query := fmt.Sprintf("INSERT INTO products(name, price, description, category) VALUES('%s', %d, '%s', '%s') RETURNING id", g_product.Name, g_product.Price, g_product.Description, g_product.Category)
+
+		fmt.Println("# INSERT QUERY: %s", query)
+		rows, err := db.Query(query)
+		if err != nil {
+			panic(err)
+		}
+
+		for rows.Next() {
+			var id int
+			err = rows.Scan(&id)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Fprintf(w, "{\"id\":%d}", id)
+
+		}
+	}
+
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Allowed", 405)
+	} else {
+		p_array := Products{}
+
+
+		fmt.Println("# Quering")
+		rows, err := db.Query("SELECT id,name,price,description,category from products")
+		if err != nil {
+			panic(err)
+		}
+
+	for rows.Next() {
+		p_product := Product{}
+
+		err = rows.Scan(&p_product.Id,&p_product.Name,&p_product.Price,&p_product.Description,&p_product.Category)
+		if err != nil {
+			panic(err)
+		}
+		p_array.Products = append(p_array.Products, p_product)
+	}
+
+	json.NewEncoder(w).Encode(p_array)
+	}
+}
