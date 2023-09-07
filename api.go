7fcd3840c0cba57116b1964d1b467e0cfce6255f
Ivan <abramenkoiv@nspk.ru>
2023-09-07 22:24:56 +0300

    post

--- api.go
+++ api.go
@@ -21,6 +21,16 @@ type Product struct {
 type Products struct {
 	Products []Product
 }
+
+
+type Order struct {
+        Id int
+        Name string
+        Amount int
+	Price int
+}
+
+
 func main() {
 	var err error
 
@@ -32,43 +42,47 @@ func main() {
 	defer db.Close()
 
 	fmt.Println("Starting...")
-	http.HandleFunc("/v2/products/:id", changeProduct)
+	http.HandleFunc("/v2/orders/add", addOrder)
 	http.HandleFunc("/v2/products/", getProducts)
 	log.Fatal(http.ListenAndServe(":7070", nil))
 
 }
-func changeProduct(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "PUT" {
+
+func addOrder(w http.ResponseWriter, r *http.Request) {
+        if r.Method != "POST" {
                 http.Error(w, "Method Not Allowed", 405)
-	} else {
-		decoder := json.NewDecoder(r.Body)
-		var g_product Product
+        } else {
+                decoder := json.NewDecoder(r.Body)
+                var g_order Order
 
-		err := decoder.Decode(&g_product)
-		if err != nil {
-			panic(err)
-		}
+                err := decoder.Decode(&g_order)
+                if err != nil {
+                        panic(err)
+                }
 
-		query := fmt.Sprintf("UPDATE products SET (name = $2, price = $3, description = $4, category = $5) WHERE id = $1 RETURNING id")
+                query := fmt.Sprintf("INSERT INTO orders(name, amount, price) VALUES('%s', %d, %d) RETURNING id",g_order.Name, g_order.Amount, g_order.Price)
 
-		fmt.Println("# UPDATE QUERY: %s", query)
-		rows, err := db.Query(query)
-		if err != nil {
-			panic(err)
-		}
+                fmt.Println("# INSERT QUERY: %s", query)
+                rows, err := db.Query(query)
+                if err != nil {
+                        panic(err)
+                }
 
-		for rows.Next() {
-			var id int
-			err = rows.Scan(&id)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Fprintf(w, "{\"id\":%d}", id)
 
-		}
-	}
+                for rows.Next() {
+                        var id int
+                        err = rows.Scan(&id)
+                        if err != nil {
+                                panic(err)
+                        }
+                        fmt.Fprintf(w, "{\"id\":%d}", id)
+
+                }
+
+        }
 
 }
+
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
@@ -96,3 +110,4 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(p_array)
 	}
 }
+
